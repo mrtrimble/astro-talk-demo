d19d2f0d370b4bc3effaa44858d29710cc87e788
Ryan Trimble <mrtrimble2@gmail.com>
2022-09-29 11:26:21 -0400

    add netlify-edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react(), svelte(), mdx(), image()]
+  integrations: [vue(), react(), svelte(), mdx(), image()],
+  output: "server",
+  adapter: netlify()
 });
