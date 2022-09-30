e3595aa37ac80e77107cfb6b25078a1acb159c1f
Ryan Trimble <mrtrimble2@gmail.com>
2022-09-30 08:34:16 -0400

    add astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react(), svelte(), mdx(), image()]
+  integrations: [vue(), react(), svelte(), mdx(), image(), compress()]
 });
