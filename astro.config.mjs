0ea693a56790146b9f59e27683a602cdad534138
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-07 10:56:54 -0400

    initial state

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
+import vue from '@astrojs/vue';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react(), svelte(), mdx(), image(), compress()]
+  integrations: [vue(), react(), svelte(), mdx(), image(), compress()],
 });
